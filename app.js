49bf89f5108a9caa9318e81526d43f08988eeb00
portelarwh <151803085+portelarwh@users.noreply.github.com>
2026-04-27 22:02:38 -0300

    chore: atualiza versao visivel do app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 (()=>{
-const APP_VERSION='v2.4.11';
+const APP_VERSION='v2.4.14';
 window.APP_VERSION=APP_VERSION;
 const STORAGE_KEY='operix_crono_maquina_v240';
 const $=id=>document.getElementById(id);
